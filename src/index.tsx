5ccd331b38bd7a09b1e8a44b003db7393834e7e8
Eric Lake <ericallenlake@gmail.com>
2022-11-10 15:50:53 -0700

    Adds global state and types

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import { App }from './App';
-
+import React, { useContext } from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { App } from "./App";
+import { AppStateProvider } from "./state/AppStateContext";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
+
+
+
 root.render(
   <React.StrictMode>
-    <App />
+    <AppStateProvider>
+      <App />
+    </AppStateProvider>
   </React.StrictMode>
 );
-
-
